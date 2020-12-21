f5015adc83b71a11483d80e1b6c580775944b24b
imuradevelopment <imuradevelopmentauth@gmail.com>
2020-12-21 14:27:08 +0900

    example

--- example/odd_or_even.py
+++ example/odd_or_even.py
@@ -1,20 +1,12 @@
 def odd_or_even():
-  for i in range(1, 6):
-    if i % 2 != 0:
-      print("{0} is odd".format(i))
-    elif i % 2 != 1:
-      print("{0} is even".format(i))
-    else:
-      print("{0} is not even and not odd.".format(i))
+    for i in range(1, 6):
+        if i % 2 != 0:
+            print("{0} is odd".format(i))
+        elif i % 2 != 1:
+            print("{0} is even".format(i))
+        else:
+            print("{0} is not even and not odd.".format(i))
+
 
 if __name__ == "__main__":
     odd_or_even()
-
-# -*- coding: utf-8 -*-
-print("こんにちは世界")
-
-str = 'jhbjkj'
-str2 = """sdvc
-b.jb"""
-
-print("{0},{1}".format(str,str2))
--- example/string_func.py
+++ example/string_func.py
@@ -1,17 +1,30 @@
+# 文字列改行
+text0 = """sdvc
+b.jb"""
+
+# エンコード指定
+# -*- coding: utf-8 -*-
+print("こんにちは世界")
+
 text1 = "abc"
+# in
 a_in_text = "a" in text1
 b_in_text = "d" in text1
 print("in_text_a {0}".format(a_in_text))
 print("in_text_d {0}".format(b_in_text))
+# 文字列結合
 text2 = "abc" + "def"
 print("abc + def {0}".format(text2))
+# 文字列繰り返し
 print("abc * 3 {0}".format("abc" * 3))
+# 文字列一部指定
 print("text2[3] {0}".format(text2[3]))
 print("text2[-1] {0}".format(text2[-1]))
 print("text2[1:4] {0}".format(text2[1:4]))
 print("text2[0:6:2] {0}".format(text2[0:6:2]))
 print("text2[0:6:3] {0}".format(text2[0:6:3]))
+# 文字列関数
 print("len(text2) {0}".format(len(text2)))
 print("min(text2) {0}".format(min(text2)))
 print("max(text2) {0}".format(max(text2)))
-print("{0},{1},{2}".format(10,"20",str(10)+"20"))
+print("{0},{1},{2}".format(10, "20", str(10)+"20"))
